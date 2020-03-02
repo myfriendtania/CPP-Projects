7eb90ced617f4239286d0e05af83f2a26edf1d93
Tania <20519161+thetalentedleo@users.noreply.github.com>
2020-03-02 14:54:01 -0600

    Create forFun.cpp

---
+++ forFun.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+    string characterName = "Tania T DAWGGGGGG";
+    string characterNewName = "Isles";
+    int characterAge;
+        characterNewName;
+    characterAge = 27;
+    characterNewName = "Isles";
+    cout << "There was once a woman named " << characterName << endl;
+    cout << "She is " << characterAge << endl;
+    cout << "She loves the name " << characterNewName << endl;
+    cout << "But she can live with being named " << characterName << endl;
+
+}
